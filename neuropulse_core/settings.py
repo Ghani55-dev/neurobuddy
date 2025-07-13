31d42692a936fd99fb0b08e0c96d014b4f41273c
Ghani55-dev <ganaganiganesh5268@gmail.com>
2025-07-13 23:27:12 +0530

    Update settings.py

--- neuropulse_core/settings.py
+++ neuropulse_core/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'neuropulse_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobboard_db',
+        'USER': 'postgres',
+        'PASSWORD': '89789',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
