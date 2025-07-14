27de06e7f45ddbba1a90e2164553eca47e7e06c7
Ganagani <ganaganiganesh5268@gmail.com>
2025-07-14 14:16:18 +0530

    Added dj-database-url and updated settings for Render DB

--- neuropulse_core/settings.py
+++ neuropulse_core/settings.py
@@ -15,6 +15,7 @@ import os
 from decouple import config
 import firebase_admin
 from firebase_admin import credentials
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,14 +97,7 @@ WSGI_APPLICATION = 'neuropulse_core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
